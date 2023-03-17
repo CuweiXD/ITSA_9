008bcd1b7867771bf3e929e8e52bf07586285d1d
勝崴 施 <4b1g0178@office.stust.edu.tw>
2023-03-18 07:54:10 +0800

    加入專案檔案。

---
+++ ITSA_9/ITSA_9.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    int x, i, ans = 0;
+
+    cin >> x;
+
+    for (i = 1; i <= x; i++) {
+
+        if (i % 3 == 0) {
+            ans = ans + i;
+        }
+    }
+
+    cout << ans << endl;
+
+}
+
+
